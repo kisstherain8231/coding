8df62c6bd865a842038570c33491595846c4233c
luyu <luyu2@chinatelecom.cn>
2019-10-16 14:20:29 +0800

    'coding'

---
+++ src/main/java/coding_048/Solution.java
@@ -0,0 +1,23 @@
+package coding_048;
+
+public class Solution {
+
+
+    public void rotate(int[][] matrix) {
+        int length = matrix.length;
+        // row
+        for (int i = 0; i < length; i++) {
+            // col
+            for (int k  = 0; k < length; k++) {
+
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/coding_053/Solution.java
+++ src/main/java/coding_053/Solution.java
@@ -3,24 +3,26 @@ package coding_053;
 class Solution {
     public int maxSubArray(int[] nums) {
         int len = nums.length;
-        int  [] [] dp = new int[len][len];
+        int  [] dp =  new int[len];
 
         int result = 0;
 
         for (int j = 0; j < len; j++) {
-            dp[j][j] = nums[j];
+            dp[j] = nums[j];
         }
 
-        result = dp[0][0];
+        result = dp[0];
         // [-2,1,-3,4,-1,2,1,-5,4]
-        for (int i = 0; i < len; i++) {
-            for (int k = i + 1; k < len; k++) {
-                dp[i][k] = dp[i][k -1] + nums[k];
+        for (int i = 1; i < len; i++) {
+           int summary = dp[i - 1] + nums[i];
 
-                if (dp[i][k] > result) {
-                    result = dp[i][k];
-                }
-            }
+           if (summary > nums[i]) {
+               dp[i] = summary;
+           }
+
+           if (result < dp[i]) {
+               result = dp[i];
+           }
         }
 
         return result;
