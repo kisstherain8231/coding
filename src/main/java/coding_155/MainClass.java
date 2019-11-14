4d0dd628614424566c0f6cff0c3882a4af39c5ca
luyu <luyu2@chinatelecom.cn>
2019-11-14 15:14:50 +0800

    'fix'

---
+++ src/main/java/coding_155/MainClass.java
@@ -0,0 +1,13 @@
+package coding_155;
+
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        MinStack solution =  new MinStack();
+
+        solution.push();
+
+    }
+
+}
---
+++ src/main/java/coding_234/Solution.java
@@ -0,0 +1,20 @@
+package coding_234;
+
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+
+        return true;
+    }
+}
