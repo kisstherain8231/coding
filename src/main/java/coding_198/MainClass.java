ccaef7591b4d3dba7e70c6b4f8d66d1c78fb38bd
luyu <luyu2@chinatelecom.cn>
2019-11-16 16:31:48 +0800

    'fix'

---
+++ src/main/java/coding_198/MainClass.java
@@ -0,0 +1,12 @@
+package coding_198;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int nums [] = {2,7,9,3,1};
+        int result = solution.rob(nums);
+        System.out.println(result);
+    }
+
+}
--- src/main/java/coding_198/Solution.java
+++ src/main/java/coding_198/Solution.java
@@ -2,4 +2,33 @@ package coding_198;
 
 public class Solution {
 
+    public int rob(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+
+        int dp[] = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], dp[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+
+            dp[i] = Math.max(dp[i - 2] + nums[i],
+                dp[i - 1]);
+        }
+
+
+        return dp[nums.length -1 ];
+    }
+
 }
