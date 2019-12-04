f5cfd4b32e08dfb888e0a959060ba51c20a98140
luyu <luyu2@chinatelecom.cn>
2019-12-04 14:15:55 +0800

    'fix'

--- src/main/java/coding_041/Solution.java
+++ src/main/java/coding_041/Solution.java
@@ -1,12 +1,64 @@
 package coding_041;
 
 public class Solution {
+
     public int firstMissingPositive(int[] nums) {
-        int missing = 0;
 
+        if (null == nums) {
+            return 1;
+        }
 
+        if(nums.length == 0) {
+            return 1;
+        }
 
+        if (nums.length == 1) {
+            if (nums[0] <= 0 ) {
+                return 1;
+            } else if (nums[0]  == 1){
+                return 2;
+            } else {
+                return 1;
+            }
+        }
+        int max = nums[0];
 
-        return  missing;
+        int min = nums[0];
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+            sum = sum + nums[i];
+        }
+
+        int targetSum = ((max - min + 1) * (min + max)) / 2;
+
+        if (max - min + 1 == nums.length) {
+            if (min == 0) {
+                return max + 1;
+            } else {
+                return 1;
+            }
+        } else {
+            if (min < 0) {
+                return targetSum - sum;
+            } else if (min > 0) {
+                if (min > 1) {
+                    return 1;
+                } else {
+                    return targetSum - sum;
+                }
+
+            } else { // min == 0
+                return targetSum - sum;
+            }
+        }
+
     }
 }
